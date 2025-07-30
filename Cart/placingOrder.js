34ce16dba67ceb8d33f433f9826c877ce310f091
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-07-30 21:21:05 +0300

    add order in DB

--- Cart/placingOrder.js
+++ Cart/placingOrder.js
@@ -20,6 +20,7 @@
 var ChippingListContainerDiv = document.getElementById("ChippingListContainer")
 var ButtonContainer = document.getElementById("ButtonContainer")
 var httpGetShippingMethods = new XMLHttpRequest()
+var chippingMethodsList = []
 function reteriveShippingMethods()
 {
     httpGetShippingMethods.open("GET","http://localhost:3000/shipping_methods")
@@ -30,13 +31,13 @@ function reteriveShippingMethods()
 httpGetShippingMethods.addEventListener("readystatechange",()=>
 {
     if ((httpGetShippingMethods.readyState == 4) && (httpGetShippingMethods.status == 200)) {
-        var chippingList = JSON.parse(httpGetShippingMethods.response)
-        console.log(chippingList)
+        chippingMethodsList = JSON.parse(httpGetShippingMethods.response)
+        console.log(chippingMethodsList)
         var label = document.createElement("label")
         label.setAttribute("for","ShippinMethod")
         label.innerHTML = `Choose SHipping Method  `
         var selectElement = document.createElement("select")
-        for (const element of chippingList) {
+        for (const element of chippingMethodsList) {
             var option = document.createElement("option")
             option.innerText =  element.name
             option.setAttribute("value",`${element.id}`)
@@ -54,8 +55,31 @@ var sendOrderButton = document.createElement("button")
 sendOrderButton.innerHTML = "sumbit"
 ButtonContainer.appendChild(sendOrderButton)
 
-ButtonContainer.addEventListener("click",()=>
+sendOrderButton.addEventListener("click",()=>
 {
-    var shippingMethod = document.querySelector("#ChippingListContainer select").value
-    console.log(shippingMethod)
+    var shippingMethodId = document.querySelector("#ChippingListContainer select").value
+    var shippingMethodOpject = chippingMethodsList.find((shippingMethod)=>
+    {
+        return shippingMethod.id == shippingMethodId
+    })
+
+    var allProcutsInCart = JSON.parse(localStorage.getItem("cart"))
+    var totalPrice = 0
+    for (const element of allProcutsInCart) {
+        totalPrice += element.price
+    }
+    totalPrice +=shippingMethodOpject.price
+    // console.log({shippingMethodOpject , allProcutsInCart , totalPrice , "UserID":Math.floor(Math.random() * 10) + 1})
+    // var Order = JSON({shippingMethodOpject , allProcutsInCart , totalPrice , "UserID":Math.floor(Math.random() * 10) + 1})
+    // console.log(Order)
+    var Order = {
+        products : allProcutsInCart,
+        shipping_method : shippingMethodOpject,
+        total_price : totalPrice,
+        user_id : Math.floor(Math.random() * 10) + 1
+    }
+    console.log(Order)
+    httpGetShippingMethods.open("POST","http://localhost:3000/orders")
+    httpGetShippingMethods.setRequestHeader('Content-Type', 'application/json')
+    httpGetShippingMethods.send(JSON.stringify(Order))
 })
