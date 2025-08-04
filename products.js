98e0eaf55d46771e29715356ee8fd4462b9e047c
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-08-05 00:39:06 +0300

    final style
    ي شباب انا زهقت والله انا عدلت شويه حاجات فى الاسيايل ناقص بس الرسبنسف جبت فى اخرى ياريت لو البشمهندسه هاجر تعمله

---
+++ products.js
@@ -0,0 +1,50 @@
+const productsContainer = document.getElementById("allProductsContainer");
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:3000/products", true);
+xhr.send();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      const products = JSON.parse(xhr.responseText);
+
+      // لو مفيش منتجات
+      if (!products.length) {
+        productsContainer.innerHTML = "<p>No products available.</p>";
+        return;
+      }
+
+      // إنشاء كارت لكل منتج
+      products.forEach((product) => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const img = document.createElement("img");
+        img.src = product.image;
+        img.alt = product.title;
+
+        const title = document.createElement("h5");
+        title.textContent = product.title;
+
+        
+        const price = document.createElement("p");
+        price.textContent = `${product.price}$`;
+
+        // زر التفاصيل
+        const link = document.createElement("a");
+        link.href = `product-details.html?id=${product.id}`;
+        link.textContent = "View Details";
+        link.className = "details-link";
+
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(price);
+        card.appendChild(link);
+
+        productsContainer.appendChild(card);
+      });
+    } else {
+      productsContainer.innerHTML = "<p>Failed to load products.</p>";
+    }
+  }
+};
